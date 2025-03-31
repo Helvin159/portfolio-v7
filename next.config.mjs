cfcf6ef096f07c7ce6330d6c63670cf11fb03b96
Helvin Rymer <55415802+Helvin159@users.noreply.github.com>
2025-03-31 11:24:39 -0400

    feat: tailwind; home hero styling

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
