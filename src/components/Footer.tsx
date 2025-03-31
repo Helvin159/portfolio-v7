7251b7e514357dbde8d9b6b34615d9781b1dbde6
Helvin Rymer <55415802+Helvin159@users.noreply.github.com>
2025-03-31 15:01:49 -0400

    feat: updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,6 +12,8 @@ import githubImg from '@/assets/images/svg/icon-github-square.svg'
 
 // Context
 import { useAuth } from '../context/auth-provider'
+import Link from 'next/link'
+import Image from 'next/image'
 
 type FooterProps = {
   name: string
@@ -25,77 +27,23 @@ const Footer = ({ name, email, linkedIn, instagram, github }: FooterProps) => {
   const loadingText = 'loading...'
   const { user } = useAuth()
 
+  console.log(user)
+
   const fName = useRef(null)
   const fEmail = useRef(null)
   const fMsg = useRef(null)
   const fPjType = useRef(null)
   const form = useRef(null)
 
-  // const sendEmail = (e: any) => {
-  //   e.preventDefault()
-
-  //   const nameInput = document.getElementById('name'),
-  //     emailInput = document.getElementById('email')
-
-  //   let msg = `
-  // 	Message:
-  // 	${fMsg.current.value ? fMsg.current.value.toString() : 'No message.'}
-
-  // 	Project Type:
-  // 	${fPjType?.current.value ? fPjType?.current?.value.toString() : 'No project type details.'}
-  // 	`
-
-  //   let templateParams = {
-  //     from_name: fName.current.value.toString(),
-  //     from_email: fEmail.current.value.toString(),
-  //     from_message: msg,
-  //   }
-
-  //   // Check if name is empty
-  //   if (fName.current.value === '' || null) {
-  //     nameInput.classList.add('error')
-  //     setTimeout(() => nameInput.classList.remove('error'), 3000)
-  //   }
-
-  //   // Check if email input is empty or doesnt match email pattern
-  //   if (fEmail.current.value === '' || null || !emailRegex.test(fEmail.current.value)) {
-  //     emailInput.classList.add('error')
-
-  //     setTimeout(() => emailInput.classList.remove('error'), 3000)
-  //   }
-
-  //   // If email and name checks, send email
-  //   if (
-  //     (((fName.current.value !== '' || null) && fEmail.current.value !== '') || null) &&
-  //     emailRegex.test(fEmail.current.value)
-  //   ) {
-  //     emailjs
-  //       .send(
-  //         process.env.REACT_APP_SERVICE_ID,
-  //         process.env.REACT_APP_TEMPLATE_ID,
-  //         templateParams,
-  //         process.env.REACT_APP_PUBLIC_KEY,
-  //       )
-  //       .then(
-  //         (res) => {
-  //           // console.log('SUCCESS!', res.status, res.text);
-  //           alert('Message sent')
-  //         },
-  //         (err) => {
-  //           // console.log('FAILED...', err);
-  //           alert('Message failed, please try again...')
-  //         },
-  //       )
-  //   }
-  // }
-
   return (
-    <div className="footer">
-      <div className="footer__content">
-        <div className="footer__content__contact__container">
-          <div className="footer__content__contact__container__form footer__content__contact__container__sizing">
-            <div className="footer__content__contact__container__form__header">
-              <p>// Drop Me A Line</p>
+    <div className="bg-light-gray m-0 pt-14 px-0 pb-0.5">
+      <div className="mx-auto w-full max-w-7xl">
+        <div className="flex flex-col-reverse flex-wrap justify-center items-center m-0 px-20 py-0 md:p-0 lg:flex-row lg:items-baseline lg:justify-evenly">
+          <div className="footer__content__contact__container__sizing">
+            <div className="">
+              <p className="text-white text-2xl italic font-light leading-10 text-left">
+                // Drop Me A Line
+              </p>
             </div>
             <form ref={form}>
               <input
@@ -104,51 +52,97 @@ const Footer = ({ name, email, linkedIn, instagram, github }: FooterProps) => {
                 name="name"
                 placeholder="YOUR NAME *"
                 ref={fName}
+                className="appearance-none border-box text-xl max-w-xl outline-0 py-0 px-2 bg-input-bg text-input-text border-2 border-input-border rounded block font-light h-14 leading-6 mb-4"
                 required
               />
-              <p>Please enter a name.</p>
+              <p className="hidden">Please enter a name.</p>
               <input
                 type="email"
                 id="email"
                 name="email"
                 placeholder="YOUR EMAIL *"
                 ref={fEmail}
+                className="appearance-none border-box text-xl max-w-xl outline-0 py-0 px-2 bg-input-bg text-input-text border-2 border-input-border rounded block font-light h-14 leading-6 mb-4"
                 required
               />
-              <p>Please enter a valid email address.</p>
+              <p className="hidden">Please enter a valid email address.</p>
               <input
                 type="text"
                 id="pjType"
                 name="pjType"
                 placeholder="WHAT TYPE OF PROJECT ARE YOU LOOKING FOR?"
+                className="appearance-none border-box text-xl max-w-xl outline-0 py-0 px-2 bg-input-bg text-input-text border-2 border-input-border rounded block font-light h-14 leading-6 mb-4"
                 ref={fPjType}
               />
-              <input type="text" id="msg" name="msg" placeholder="WRITE A MESSAGE..." ref={fMsg} />
+              <input
+                type="text"
+                id="msg"
+                name="msg"
+                placeholder="WRITE A MESSAGE..."
+                className="appearance-none border-box text-xl max-w-xl outline-0 py-0 px-2 bg-input-bg text-input-text border-2 border-input-border rounded block font-light h-14 leading-6 mb-4"
+                ref={fMsg}
+              />
             </form>
-            <button type="submit">send</button>
+            <button
+              type="submit"
+              className="bg-white border-2 shadow-btn-highlight text-black cursor-pointer h-12 leading-6 my-12 mx-auto uppercase appearance-none border-box text-xl max-w-xl outline-0 py-0 px-2 w-full"
+            >
+              send
+            </button>
           </div>
           <div className="footer__content__contact__container__contact footer__content__contact__container__sizing">
             <div className="footer__content__contact__container__contact__header">
-              <p>// Lets Colaborate</p>
+              <p className="text-white text-2xl italic font-light leading-10 text-left">
+                // Lets Colaborate
+              </p>
             </div>
             <div className="contact-details">
-              <h2>{name ? name : loadingText}</h2>
+              <h2 className="text-white text-4xl mt-4 mb-6 font-medium leading-11">
+                {name ? name : loadingText}
+              </h2>
               <div className="links">
-                <a href="tel:9295236682">(929) 523-6682</a>
-                <a href={`mailto:${email ? email : loadingText}`}>{email ? email : loadingText}</a>
+                <Link
+                  className="text-white block text-2xl font-medium leading-8 decoration-0"
+                  href="tel:9295236682"
+                >
+                  (929) 523-6682
+                </Link>
+                <Link
+                  className="text-white block text-2xl font-medium leading-8 decoration-0"
+                  href={`mailto:${email ? email : loadingText}`}
+                >
+                  {email ? email : loadingText}
+                </Link>
               </div>
             </div>
-            <div className="social-media">
-              <p>Connect with me on social media!</p>
-              <a href={linkedIn} target="_blank" rel="noreferrer">
-                <img src={linkedInImg} alt="LinkedIn" />
-              </a>
-              <a href={instagram} target="_blank" rel="noreferrer">
-                <img src={instagramImg} alt="Instagram" />
-              </a>
-              <a href={github} target="_blank" rel="noreferrer">
-                <img src={githubImg} alt="Github" />
-              </a>
+            <div className="social-media pt-6 pb-2 sm:pt-10">
+              <p className="text-md font-semibold">Connect with me on social media!</p>
+              <div className="flex flex-wrap gap-x-4 py-0.5">
+                <Link
+                  className="text-left inline-block"
+                  href={linkedIn}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Image src={linkedInImg} alt="LinkedIn" />
+                </Link>
+                <Link
+                  className="text-left inline-block"
+                  href={instagram}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Image src={instagramImg} alt="Instagram" />
+                </Link>
+                <Link
+                  className="text-left inline-block"
+                  href={github}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Image src={githubImg} alt="Github" />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
---
+++ src/utils/send-email.ts
@@ -0,0 +1,35 @@
+import emailjs from 'emailjs-com'
+
+// Utils
+// import { emailRegex } from '@/utils/utils'
+
+/**
+ *  Sends email using emailJs
+ */
+export default function sendEmail() {
+  let templateParams = {
+    from_name: '',
+    from_email: '',
+    from_message: '',
+  }
+
+  // If email and name checks, send email
+
+  emailjs
+    .send(
+      process.env.EMAIL_JS_SERVICE_ID ?? '',
+      process.env.EMAIL_JS_TEMPLATE_ID ?? '',
+      templateParams,
+      process.env.EMAIL_JS_PUBLIC_KEY ?? '',
+    )
+    .then(
+      (res) => {
+        // console.log('SUCCESS!', res.status, res.text);
+        alert('Message sent')
+      },
+      (err) => {
+        // console.log('FAILED...', err);
+        alert('Message failed, please try again...')
+      },
+    )
+}
