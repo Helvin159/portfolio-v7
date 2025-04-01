c03a153ee4b9defd6655d4286de2a9cf4787ed68
Helvin Rymer <55415802+Helvin159@users.noreply.github.com>
2025-03-31 20:59:02 -0400

    refactor: import organization

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
+// Context
+import { AuthProvider } from '@/context/auth-provider'
+import { MobileMenuProvider } from '@/context/mobile-menu-context'
+
+// Components
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 
 // CSS
 import '@/assets/css/styles.css'
 import '@/assets/css/tailwind/output.css'
-import { MobileMenuProvider } from '../../context/mobile-menu-context'
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
-import { AuthProvider } from '@/context/auth-provider'
-import loadPayload from '../../utils/load-payload'
 
 export const metadata = async () => {
   return {
